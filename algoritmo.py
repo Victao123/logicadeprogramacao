338ceb7c8293f23fa0d0a9023c9b1e5e6f3a7cac
Victor Hugo Alves Pires <100798612+Victao123@users.noreply.github.com>
2025-08-13 18:08:25 -0300

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -1,11 +1,15 @@
-#comentário de uma linha  
+comentário de uma linha  
 
 
 
-primeiro_numero = input("digite um número inteiro: ")
-primeiro_numero = int(primeiro_numero)
+primeiro_numero = int(input("Digite o segundo numero"))
+segundo_numero=int(input("Digite outro número inteiro: "))
 
 
-segundo_numero = input("digite outro número inteiro: ")
+while primeiro_numero != segundo_numero:
+    if primeiro_numero > segundo_numero:
+        primeiro_numero = primeiro_numero - segundo_numero
+    else:
+        segundo_numero = segundo_numero - primeiro_numero
 
-print(primeiro_numero +  segundo_numero)
+print("Os números ficaram iguais:", primeiro_numero, segundo_numero )
