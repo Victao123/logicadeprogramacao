c7467e41d7c35594fef90e0ee59e525c1c64c743
Victor Hugo Alves Pires <100798612+Victao123@users.noreply.github.com>
2025-08-27 17:24:57 -0300

    Update adivinhe_o_numero4.py

--- adivinhe_o_numero4.py
+++ adivinhe_o_numero4.py
@@ -8,6 +8,7 @@ def jogar_adivinhacao():
   numero_secreto = random.randint(1, 100)
   tentativas = 0
   acertou = False
+  tentativas_passadas = []
 
   print(f"\nOlá, {nome_jogador}! Bem-vindo ao jogo de adivinhação ")
   print("Tente adivinhar um número entre 1 e 100.")
@@ -15,14 +16,15 @@ def jogar_adivinhacao():
   while not acertou:
     tentativa = int(input("Digite seu palpite: "))
     tentativas += 1
-
+    tentativas_passadas.append(tentativa)
     if tentativa == numero_secreto:
       acertou = True
+      
       print(f"Parabéns, {nome_jogador}! Você acertou o número em {tentativas} tentativas.")
+      print(tentativas_passadas)
     elif tentativa < numero_secreto:
       print("Tente um número maior.")
     else:
       print("Tente um número menor.")
 
 jogar_adivinhacao()
-
